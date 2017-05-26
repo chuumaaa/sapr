f110dccf14aa2e664b8d21838e349e6be1e682a2
Kasyanenko Anton <antkasyak@gmail.com>
2017-05-26 16:04:44 +0300

    Add sign-in with cookies

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   get 'users/new'
 
   root 'pages#home'
   match '/signup', to:'users#new', via:'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
